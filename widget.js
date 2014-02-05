9034c699694b5d54a1fcf639eb7310c07332f43a
Thomas Davis <thomasalwyndavis@gmail.com>
2014-02-06 00:05:36 +1000

    started as minimized

--- widget.js
+++ widget.js
@@ -50,6 +50,7 @@ var _tfrce_config = (typeof tfrce_config  !== 'undefined') ? tfrce_config  : {};
   widget_config.greeting = widget_config.greeting || 'Dear Internet';
   widget_config.debug = widget_config.debug || false;
   widget_config.callOnly = widget_config.callOnly || true;
+  widget_config.startAsMinimized = widget_config.startAsMinimized || true;
   widget_config.disableDate = widget_config.disableDate || false;
   widget_config.campaign = widget_config.campaign || 'thedaywefightback';
   widget_config.cookieTimeout = widget_config.cookieTimeout || 172800;
@@ -138,7 +139,10 @@ var _tfrce_config = (typeof tfrce_config  !== 'undefined') ? tfrce_config  : {};
       },
       minimized: false,
       show: function (options) {
-
+        var cookie = getCookie(active_campaign.cookieName);
+        if(widget_config.startAsMinimized && cookie === null) {
+          this.minimized = true;
+        }
         var style = active_campaign.styles[active_campaign.config.show_style]
         
         if(style.overlay) {
@@ -193,7 +197,6 @@ var _tfrce_config = (typeof tfrce_config  !== 'undefined') ? tfrce_config  : {};
           selectedLocation = options.location.country.iso_code;
         }
         var firstTime = true;
-        var cookie = getCookie(active_campaign.cookieName);
         if(cookie !== null) {
           firstTime = false;
         }
